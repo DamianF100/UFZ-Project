0f59e68049cd6b792f9fc496d61dfdd9e92d5e65
JohannesKowal <johanneslucakowal@gmail.com>
2026-03-11 14:29:05 +0100

    Sliders added to interface

--- QT_Project/mainwindow.cpp
+++ QT_Project/mainwindow.cpp
@@ -13,18 +13,16 @@ uniform_int_distribution<int> randist_1_27(1,27);
 int speed_timer = 50;
 
 // Definitions of the size
-const int HEIGHT       = 500; //Height of the the entire simulated area
-const int WIDTH        = 500; //Width of the entire simulated ares
-const int SIZE_COLONIE = 200;  //Radius of the simulted colonie - It is a square with this line size
+const int HEIGHT       = 200; //Height of the the entire simulated area
+const int WIDTH        = 200; //Width of the entire simulated ares
+const int SIZE_COLONIE = 75;  //Radius of the simulted colonie - It is a square with this line size
 
 // Paremeter for the neighbour relation
 int rest_time = 18; //Which time after breeding the individuum cannot breed
-int option = 1; //switch between the different neigbour relation - either they start direct breeding or the index increases faster
 double BREED_TRESH = 24; //Threshold after which a individuum starts breeding
-int    NEIG_TRESH  = 28; //After how many breeding neighbours an individuum starts breeding
-int    NEIG_DIST   = 8; //the number of indivuum which are considered for the counter - the # is (NEIG_DIST*2)^2
-int    NEIG_EFF    = 15; //How much the index is increased if the threshold is activated
 
+// interesting parameter combinations: (24,40,10,10)
+// interesting parameter combinations: (24,30,8,5)
 //Arrays to simulate
 int    BREEDING[HEIGHT][WIDTH]; //Either 0 if an individuum is not breeding - 1 if it is
 double BREEDING_INDEX[HEIGHT][WIDTH]; //Index how high an individuum is in the cycle - 1 t0 27
@@ -35,24 +33,43 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {   //Initialises the plot
     ui->setupUi(this);
-    ui->Plot->SetYcells(HEIGHT);
-    ui->Plot->SetXcells(WIDTH);
+    ui->COLLONY->SetYcells(HEIGHT);
+    ui->COLLONY->SetXcells(WIDTH);
 
     //Plot colors
-    ui->Plot->LegendAdd(0, "White");
-    ui->Plot->LegendAdd(1, "Red");
-    ui->Plot->LegendAdd(2, "Green");
+    ui->COLLONY->LegendAdd(0, "White");
+    ui->COLLONY->LegendAdd(1, "Red");
+    ui->COLLONY->LegendAdd(2, "Green");
 
     running = false;
-
     connect(&timer, &QTimer::timeout, this, & MainWindow::on_STEP_clicked);
-
     timer.start(speed_timer);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// Options
+
+void MainWindow::on_NEIG_TRESH_sliderMoved(int position)
+{
+    NEIG_TRESH = position;
+}
+
+
+void MainWindow::on_NEI_DIST_sliderMoved(int position)
+{
+    NEIG_DIST = position;
+}
+
+
+void MainWindow::on_NEIG_EFF_sliderMoved(int position)
+{
+    NEIG_EFF = position;
 }
 
 //Initialise the arrays
@@ -86,10 +103,10 @@ void MainWindow::on_INIT_clicked()
     //Give the plot
     for(int y = 0; y < HEIGHT; y++){
         for(int x = 0; x < WIDTH; x++){
-            ui->Plot->SetXY(y, x, BREEDING[y][x]);
+            ui->COLLONY->SetXY(y, x, BREEDING[y][x]);
         }
     }
-    ui->Plot->replot();
+    ui->COLLONY->replot();
 }
 
 //One time step
@@ -113,7 +130,7 @@ void MainWindow::on_STEP_clicked(){
                     }
                 }
 
-                NEIGHBOURS[x][y] = COUNT_NEIG;
+                NEIGHBOURS[y][x] = COUNT_NEIG;
             }
         }
     }
@@ -125,18 +142,7 @@ void MainWindow::on_STEP_clicked(){
 
             if( (y - HEIGHT/2)*(y - HEIGHT/2) + (x - WIDTH/2)*(x - WIDTH/2) <= SIZE_COLONIE*SIZE_COLONIE ){
 
-                if(option == 1){
-
-                    if(NEIGHBOURS[x][y] > NEIG_TRESH && BREEDING_INDEX[y][x] >  rest_time  && BREEDING[y][x] != 2){ //If the neighbours threshold is met, it is not in rest time and not breeding
-                        BREEDING_INDEX[y][x] = BREED_TRESH; //set directly to breeding
-                        
-                    }
-                    else{
-                        BREEDING_INDEX[y][x]++;
-                    }
-                }
-                else{ //TODO be updated - check the logic
-                         if (BREEDING_INDEX[y][x] < BREED_TRESH - NEIG_EFF){
+                    if (BREEDING_INDEX[y][x] < BREED_TRESH - NEIG_EFF){
                         BREEDING_INDEX[y][x] = BREEDING_INDEX[y][x] + NEIG_EFF;
                       }
                      else if(BREEDING_INDEX[y][x] >BREED_TRESH - NEIG_EFF && BREEDING_INDEX[y][x] < BREED_TRESH){
@@ -146,7 +152,6 @@ void MainWindow::on_STEP_clicked(){
                          BREEDING_INDEX[y][x] ++;
                     }
                       BREEDING_INDEX[y][x] ++;
-                }
 
                 // Updates the breeding grid
                 if(BREEDING_INDEX[y][x] > 27){
@@ -164,10 +169,10 @@ void MainWindow::on_STEP_clicked(){
     for(int y = 0; y < HEIGHT; y++){
         for(int x = 0; x < WIDTH; x++){
 
-            ui->Plot->SetXY(y, x, BREEDING[y][x]);
+            ui->COLLONY->SetXY(y, x, BREEDING[y][x]);
         }
     }
-    ui->Plot->replot();
+    ui->COLLONY->replot();
 }
 
 void MainWindow::on_RUN_clicked()
